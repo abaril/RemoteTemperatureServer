a6a188736e84e571684ac9246e96ed4863c60ca2
Allan Baril <allan.baril@gmail.com>
2012-01-07 12:04:51 -0500

    AB - Made available through MIT license

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+// Copyright (c) 2012 Allan Baril
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or 
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 var winston = require("winston");
 var udpserver = require("./udpserver");
 var temperatures = require("./temperatures");
@@ -24,7 +41,7 @@ winston.add(winston.transports.File, {
 winston.add(winston.transports.Loggly, {
 	"level": "info",
 	"subdomain": "xtremelabs",
-	"inputToken": "5ad40ba6-e3b6-4ab6-8682-77d7d771e623"
+	"inputToken": "loggly_token"
 });
 winston.info("Settings = " + JSON.stringify(settings));
 
